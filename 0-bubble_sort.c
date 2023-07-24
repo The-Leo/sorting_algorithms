8ea4dfd4c278bb8c0cdfd18d88cfaa4e1480b9b6
Leonard Odoi <odoi.leonard1@gmail.com>
2023-07-24 16:57:07 -0700

    Swap variables

---
+++ 0-bubble_sort.c
@@ -0,0 +1,16 @@
+#include "sort.h"
+
+/**
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
