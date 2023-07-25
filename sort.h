31c04fa8ab25ff41e078619f9bdb580786ad776d
Leonard Odoi <odoi.leonard1@gmail.com>
2023-07-25 16:58:06 -0700

    Enumerate boolean values

--- sort.h
+++ sort.h
@@ -8,6 +8,16 @@
 #define UP 0
 #define DOWN 1
 
+/**
+ * enum bool - Enumeration of Boolean values.
+ * @false: Equals 0.
+ * @true: Equals 1.
+ */
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
 
 
 
