eaf2c851de0b4b0c906d90bdd87b103742300b4d
Leonard Odoi <odoi.leonard1@gmail.com>
2023-07-25 16:53:21 -0700

    Include header files and macros

--- sort.h
+++ sort.h
@@ -1,5 +1,12 @@
 #ifndef SORT_H
 #define SORT_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Comparison direction macros for bitonic sort */
+#define UP 0
+#define DOWN 1
+
 
 #endif /* SORT_H */
