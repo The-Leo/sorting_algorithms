f825f2104e6924f690e10910d476341636987531
Leonard Odoi <odoi.leonard1@gmail.com>
2023-07-25 16:45:41 -0700

    Sort an array of integers in ascending order

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -14,3 +14,33 @@ void swap_ints(int *a, int *b)
 	*b = tmp;
 }
 
+/**
+ * bubble_sort - Sort an array of integers in ascending order.
+ * @array: An array of integers to sort.
+ * @size: The size of the array.
+ *
+ * Description: Prints the array after each swap.
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, len = size;
+	bool bubbly = false;
+
+	if (array == NULL || size < 2)
+		return;
+
+	while (bubbly == false)
+	{
+		bubbly = true;
+		for (i = 0; i < len - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				swap_ints(array + i, array + i + 1);
+				print_array(array, size);
+				bubbly = false;
+			}
+		}
+		len--;
+	}
+}
