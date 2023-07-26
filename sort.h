5444e8443b96e1f4634f098b02ee581582d24cad
Leonard Odoi <odoi.leonard1@gmail.com>
2023-07-25 17:01:57 -0700

    Include doubly linked list node

--- sort.h
+++ sort.h
@@ -19,7 +19,19 @@ typedef enum bool
 	true
 } bool;
 
-
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
 
 /* Sorting algoritms */
 void bubble_sort(int *array, size_t size);
